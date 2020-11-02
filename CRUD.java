0df7f64cc34fb1fea5df54d051f11150f833d935
Sanstark <47468504+Sanstark@users.noreply.github.com>
2020-11-03 01:36:19 +0530

    Add files via upload

---
+++ CRUD.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+
+public interface CRUD {
+    public List<Employee> readDataFromDatabaseToObject() throws CustomException, SQLException;
+
+    public int insertDataToEmployeeDB(String name, char gender, double salary, Date date, long phone, String address, String dept) throws SQLException;
+
+    public void updateData(String column, String name, String value) throws CustomException, SQLException;
+
+    public void deleteData();
+}
